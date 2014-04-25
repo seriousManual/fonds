737d0fec9c12e9f00a4f33e41c5c5b3cc1506e35
zaphod1984 <zaphod84@gmx.de>
2014-04-25 16:14:22 +0200

    improved monthly test

--- tests/unit/MonthlyPeriodicPayment.tests.js
+++ tests/unit/MonthlyPeriodicPayment.tests.js
@@ -28,19 +28,25 @@ describe('MonthlyPeriodicPayment', function() {
         var a = new MonthlyPeriodicPayment({
             value: 100,
             start: '2014-01-01',
-            end: '2014-01-31',
+            end: '2014-05-31',
             dates: [1, 2, 3]
         });
 
         expect(a.validatePaymentDate(moment('2013-12-01'))).to.be.false;
+        
         expect(a.validatePaymentDate(moment('2014-01-01'))).to.be.true;
         expect(a.validatePaymentDate(moment('2014-01-02'))).to.be.true;
         expect(a.validatePaymentDate(moment('2014-01-03'))).to.be.true;
         expect(a.validatePaymentDate(moment('2014-01-04'))).to.be.false;
 
-        expect(a.validatePaymentDate(moment('2014-02-01'))).to.be.false;
-        expect(a.validatePaymentDate(moment('2014-02-02'))).to.be.false;
-        expect(a.validatePaymentDate(moment('2014-02-03'))).to.be.false;
+        expect(a.validatePaymentDate(moment('2014-02-01'))).to.be.true;
+        expect(a.validatePaymentDate(moment('2014-03-02'))).to.be.true;
+        expect(a.validatePaymentDate(moment('2014-02-03'))).to.be.true;
         expect(a.validatePaymentDate(moment('2014-02-04'))).to.be.false;
+
+        expect(a.validatePaymentDate(moment('2014-06-01'))).to.be.false;
+        expect(a.validatePaymentDate(moment('2014-06-02'))).to.be.false;
+        expect(a.validatePaymentDate(moment('2014-06-03'))).to.be.false;
+        expect(a.validatePaymentDate(moment('2014-06-04'))).to.be.false;
     });
 });
