3564245e154ff211a70c8628697f6904279157d5
zaphod1984 <zaphod84@gmx.de>
2014-04-25 16:12:47 +0200

    implemented validate paymentdate for MonthlyPeriodicPayment

--- tests/unit/MonthlyPeriodicPayment.tests.js
+++ tests/unit/MonthlyPeriodicPayment.tests.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect;
+var moment = require('moment');
 
 var MonthlyPeriodicPayment = require('../../lib/MonthlyPeriodicPayment');
 
@@ -21,5 +22,25 @@ describe('MonthlyPeriodicPayment', function() {
         expect(a.start().isValid()).to.be.true;
         expect(a.end().isValid()).to.be.true;
         expect(a.dates()).to.deep.equal([1,2,3]);
+    });
+
+    it('should validate paymentdate', function() {
+        var a = new MonthlyPeriodicPayment({
+            value: 100,
+            start: '2014-01-01',
+            end: '2014-01-31',
+            dates: [1, 2, 3]
+        });
+
+        expect(a.validatePaymentDate(moment('2013-12-01'))).to.be.false;
+        expect(a.validatePaymentDate(moment('2014-01-01'))).to.be.true;
+        expect(a.validatePaymentDate(moment('2014-01-02'))).to.be.true;
+        expect(a.validatePaymentDate(moment('2014-01-03'))).to.be.true;
+        expect(a.validatePaymentDate(moment('2014-01-04'))).to.be.false;
+
+        expect(a.validatePaymentDate(moment('2014-02-01'))).to.be.false;
+        expect(a.validatePaymentDate(moment('2014-02-02'))).to.be.false;
+        expect(a.validatePaymentDate(moment('2014-02-03'))).to.be.false;
+        expect(a.validatePaymentDate(moment('2014-02-04'))).to.be.false;
     });
 });
