12e8a9ca2dce127fd74b61778d4f7fb334b179da
zaphod1984 <zaphod84@gmx.de>
2014-04-25 15:52:08 +0200

    implemented validate paymentdate for oneTime

--- tests/unit/OneTimePayment.tests.js
+++ tests/unit/OneTimePayment.tests.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect;
+var moment = require('moment');
 
 var OneTimePayment = require('../../lib/OneTimePayment');
 
@@ -18,4 +19,15 @@ describe('OneTimePayment', function() {
         expect(a.value()).to.equal(100);
         expect(a.date().format('YYYY-MM-DD')).to.deep.equal('2014-01-01');
     });
+
+    it('should validate payments', function() {
+        var a = new OneTimePayment({
+            value: 100,
+            date: '2014-01-01'
+        });
+
+        expect(a.validatePaymentDate(moment('2014-01-01'))).to.be.true;
+        expect(a.validatePaymentDate(moment('2014-01-02'))).to.be.false;
+        expect(a.validatePaymentDate(moment('2011-01-01'))).to.be.false;
+    })
 });
