7c610dbd30d77904084486c1a754f8e41dbcc5a3
zaphod1984 <zaphod84@gmx.de>
2014-04-26 07:47:54 +0200

    added Plan, added tests

---
+++ tests/unit/Plan.tests.js
@@ -0,0 +1,88 @@
+var expect = require('chai').expect;
+
+var Plan = require('../../lib/Plan');
+
+var Payment = require('../../lib/payments/Payment');
+var PeriodicPayment = require('../../lib/payments/PeriodicPayment');
+var MonthlyPeriodicPayment = require('../../lib/payments/MonthlyPeriodicPayment');
+var YearlyPeriodicPayment = require('../../lib/payments/YearlyPeriodicPayment');
+var OneTimePayment = require('../../lib/payments/OneTimePayment');
+
+describe('Plan', function() {
+    it('should throw', function() {
+        expect(function() {
+            new Plan();
+        }).to.throw(/undefined value not allowed/);
+
+        expect(function() {
+            new Plan({});
+        }).to.throw(/undefined value not allowed/);
+
+        expect(function() {
+            new Plan({
+                start: '2014-01-01'
+            });
+        }).to.throw(/not an array: undefined/);
+
+        expect(function() {
+            new Plan({
+                start: '2014-01-01',
+                payments: []
+            });
+        }).to.throw(/empty array not allowed/);
+
+        expect(function() {
+            new Plan({
+                start: '2014-01-01',
+                end: '2010-01-01',
+                payments: [{
+                    type: 'monthly',
+                    start: '2014-01-01',
+                    dates:[1, 15],
+                    value: 100
+                }]
+            });
+        }).to.throw(/2010-01-01 should be greater\/equals 2014-01-01/);
+
+        expect(function() {
+            new Plan({
+                start: '2014-01-01',
+                end: '2010-01-01',
+                payments: [{
+                    type: 'foo'
+                }]
+            });
+        }).to.throw(/could not identify payment of type: foo/);
+    });
+
+    it('should create payments', function() {
+        var a = new Plan({
+            start: '2014-01-01',
+            payments: [
+                {
+                    type: 'monthly',
+                    start: '2014-01-01',
+                    dates:[1, 15],
+                    value: 100
+                },
+                {
+                    type: 'yearly',
+                    start: '2014-01-01',
+                    dates:[[1,1]],
+                    value: 100
+                },
+                {
+                    type: 'oneTime',
+                    date: '2014-01-01',
+                    value: 100
+                }
+            ]
+        });
+
+        var payments = a.payments();
+    
+        expect(payments[0]).to.be.instanceOf(MonthlyPeriodicPayment);
+        expect(payments[1]).to.be.instanceOf(YearlyPeriodicPayment);
+        expect(payments[2]).to.be.instanceOf(OneTimePayment);
+    });
+});
