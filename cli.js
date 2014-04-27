5cb8ed4f40be808dbb52147999598fd6f04bca7a
zaphod1984 <zaphod84@gmx.de>
2014-04-27 19:38:50 +0200

    added cli interface

---
+++ cli.js
@@ -0,0 +1,52 @@
+var fs = require('fs');
+
+var args = require('optimist').argv;
+var Table = require('cli-table');
+
+var Plan = require('./lib/Plan');
+var Summary = require('./lib/Summary');
+
+var configFile = args._[0];
+
+if (!configFile) {
+    console.log('missing config file');
+    process.exit(1);
+}
+
+var plan, data;
+try {
+    if (configFile.match(/\.json/)) {
+        data = fs.readFileSync(configFile).toString();
+        data = JSON.parse(data);
+    } else {
+        data = require(configFile);
+    }
+
+    plan = new Plan(data);
+} catch(error) {
+    console.log('plan definition error: ' + error.message);
+    process.exit(1);
+}
+
+new Summary(plan)
+.createSummary(function(error, summary) {
+    var table = new Table({
+        head: ['', 'comment', 'value (€)', 'fee (€)']
+    });
+
+    summary.payments.forEach(function(payment) {
+        var row = {};
+        row[payment.date] = [
+            payment.comment,
+            payment.value,
+            payment.fee
+        ];
+
+        table.push(row);
+    });
+
+    table.push([]);
+    table.push({'sum': ['', summary.invested, summary.fee]})
+
+    console.log(table.toString());
+});
