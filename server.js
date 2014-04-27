19b6c9b25ac1a4b0904612410754008bac9b9197
zaphod1984 <zaphod84@gmx.de>
2014-04-27 20:33:10 +0200

    added plan parameter for server

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var http = require('http');
 var spawn = require('child_process').spawn;
+var fs = require('fs');
 
 var args = require('optimist').argv;
 
 var port = args.port || 8080;
+var plan = args.plan;
+
+if (!plan || !fs.existsSync(plan)) {
+    console.log('plan not found or plan invalid: ' + plan);
+    process.exit(1);
+}
 
 http.createServer(function(rew, res) {
-    spawn('node', ['./bin/cli.js', './config/user.mer.js', '--style', 'blank'], {
+    spawn('node', ['./bin/cli.js', plan, '--style', 'blank'], {
         cwd: process.cwd(),
         env: process.env
     }).stdout.pipe(res);
